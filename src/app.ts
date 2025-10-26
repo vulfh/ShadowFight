8b8ab0c1e9e64ba3f551f471e7b569bb2c352943
vulfh <Miyamoto1977!>
2025-10-26 22:49:48 +0200

    fix bug 110 - play fightlist not  always work

--- src/app.ts
+++ src/app.ts
@@ -432,9 +432,6 @@ export class KravMagaTrainerApp {
     this.enableConfigurationControls()
     this.updateSessionUI()
     
-    // Clear current fight list from storage when session stops
-    this.fightListManager.clearCurrentFightList();
-    
     this.showNotification({
       message: INFO_MESSAGES.SESSION_STOPPED,
       type: NOTIFICATION_TYPES.INFO
@@ -442,12 +439,9 @@ export class KravMagaTrainerApp {
   }
 
   private handleSessionComplete(): void {
-    // Session completed naturally - behave exactly like Stop button was pressed
+    // Session completed naturally
     this.enableConfigurationControls()
     this.updateSessionUI()
-    
-    // Clear current fight list from storage when session completes
-    this.fightListManager.clearCurrentFightList();
     
     this.showNotification({
       message: SUCCESS_MESSAGES.SESSION_COMPLETED,
